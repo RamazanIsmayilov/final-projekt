41d7262094f2349c3d0de2ed73d21809809c6a8d
Ramazan <ismayiloff522@gmail.com>
2024-05-17 17:25:49 +0400

    api

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -7,7 +7,8 @@ export const ProductContext = createContext();
 export const ProductProvider = ({ children }) => {
     const [productdata, setProductdata] = useState([]);
     useEffect(() => {
-        axios.get("http://127.0.0.1:5500/src/data/product.json")
+        // axios.get("http://127.0.0.1:5500/src/data/product.json")
+        axios.get("https://mocki.io/v1/4f5e7faa-1683-4622-ae46-40f8e48b54ec")
 
             .then(res => setProductdata(res.data));
     }, [])
